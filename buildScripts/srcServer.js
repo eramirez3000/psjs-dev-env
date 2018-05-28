691ba2f01dd3993684340476bae46fbdfefde778
Edilberto Ramirez <eramirez@nsel-clnsa.com.ni>
2018-05-28 16:23:12 -0600

    Automatización con npm-script

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -6,7 +6,7 @@ var open = require('open');
 
 var port = 3000;
 var app = express();
- 
+//
 
 app.get('/',function (req,res) {
 res.sendFile(path.join(__dirname,'../src/index.html'));
---
+++ buildScripts/startMessage.js
@@ -0,0 +1,3 @@
+var chalk = require('chalk');
+// Especificamos el color del menj\saje en la consola
+console.log(chalk.green('Starting app in dev mode....... \n'));
