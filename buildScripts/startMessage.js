ff4c8a55fce81bd66f883f34837d856c394d797e
Edilberto Ramirez <eramirez@nsel-clnsa.com.ni>
2018-05-28 17:03:15 -0600

    Transpiling Babel

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -1,3 +1,5 @@
-var chalk = require('chalk');
+//var chalk = require('chalk');
+//uusing feature js lasted
+import chalk from 'chalk';
 // Especificamos el color del menj\saje en la consola
 console.log(chalk.green('Starting app in dev mode....... \n'));
